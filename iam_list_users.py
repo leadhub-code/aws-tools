4b3fbc45d5c48b451d807aa161084b8fb255fe55
Petr Messner <petr.messner@gmail.com>
2017-07-14 14:02:34 +0200

    iam_list_users: change output structure

--- iam_list_users.py
+++ iam_list_users.py
@@ -16,9 +16,15 @@ def main():
     iam = boto3.resource('iam')
     all_users = list(iam.users.all())
     with multiprocessing.Pool(32) as pool:
-        users_data = pool.map(dump_user, [u.name for u in all_users])
-    data = {'account_id': get_account_id(), 'users': users_data}
+        users_data = to_dict(pool.map(dump_user, [u.name for u in all_users]))
+    data = {'account_id': get_account_id(), 'iam_users': users_data}
     yaml.dump(data, sys.stdout, indent=4, default_flow_style=False)
+
+
+def to_dict(items):
+    items = list(items)
+    assert len(set(k for k, v in items)) == len(items)
+    return dict(items)
 
 
 def get_account_id():
@@ -29,33 +35,31 @@ def dump_user(user_name):
     iam = boto3.resource('iam')
     user = iam.User(user_name)
     assert user.user_name == user.name
-    return {
+    return user.name, {
         'user_id': user.user_id,
-        'name': user.name,
         'create_date': user.create_date,
         'arn': user.arn,
         'path': user.path,
-        'access_keys': [dump_access_key(ak) for ak in user.access_keys.all()],
-        'attached_policies': [dump_policy(ap) for ap in user.attached_policies.all()],
-        'groups': [dump_group(g) for g in user.groups.all()],
+        'access_keys': to_dict(dump_access_key(ak) for ak in user.access_keys.all()),
+        'attached_policies': to_dict(dump_policy(ap) for ap in user.attached_policies.all()),
+        'groups': to_dict(dump_group(g) for g in user.groups.all()),
         'mfa_devices': [repr(d) for d in user.mfa_devices.all()],
-        'policies': [dump_user_policy(p) for p in user.policies.all()],
+        'policies': to_dict(dump_user_policy(p) for p in user.policies.all()),
         'signing_certificates': [repr(sc) for sc in user.signing_certificates.all()],
     }
 
 
 def dump_access_key(ak):
     assert ak.id == ak.access_key_id
-    return {
-        'access_key_id': ak.access_key_id,
+    return ak.access_key_id, {
         'crate_date': ak.create_date,
         'status': ak.status,
     }
 
 
 def dump_group(g):
-    return {
-        'group_name': g.group_name,
+    assert g.group_name == g.name
+    return g.name, {
         'group_id': g.group_id,
         'arn': g.arn,
     }
@@ -63,8 +67,7 @@ def dump_group(g):
 
 def dump_policy(p):
     assert p.__class__.__name__ == 'iam.Policy', repr(p.__class__.__name__)
-    return {
-        'arn': p.arn,
+    return p.arn, {
         'create_date': p.create_date,
         'path': p.path,
         'policy_id': p.policy_id,
@@ -88,15 +91,14 @@ def dump_policy_version(pv):
 def dump_user_policy(p):
     assert p.__class__.__name__ == 'iam.UserPolicy', repr(p.__class__.__name__)
     assert p.policy_name == p.name
-    return {
-        'policy_name': p.policy_name,
+    return p.policy_name, {
         'policy_document': p.policy_document,
     }
 
 
 def setup_logging():
     logging.basicConfig(
-        format='> [%(process)d] %(name)-30s %(levelname)5s: %(message)s',
+        format='> [%(process)d] %(name)-26s %(levelname)s: %(message)s',
         level=logging.INFO)
 
 
